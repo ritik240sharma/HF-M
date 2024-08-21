f7518611297ad3f7322ea1d04058b2fd5df56fb0
Ritik <ritik240gwalior@gmail.com>
2024-08-21 17:03:07 +0530

    created basic structure of folder

---
+++ Backend/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app=express();
+const port=5000
+app.get("/",(req, res)=>{
+    res.send("Welcome to hosel and fee managment")
+})
+app.listen(port,()=>{
+    console.log("server started")
+})
