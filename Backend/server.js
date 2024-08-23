6d53a5b87b4a84da4632fef3d8b65b92f5572da5
Akshat Bhardwaj <akshatbhardwaj019@gmail.com>
2024-08-23 22:07:18 +0530

    std

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app=express();
 const port=5000
 app.get("/",(req, res)=>{
-    res.send("Welcome to student accomodation")
+    res.send("Welcome to student accomodation. We will provide a faster and efficient way to allot you a hostel.")
 })
 app.listen(port,()=>{
     console.log("server started on port 9000")
