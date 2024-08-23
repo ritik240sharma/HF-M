c395c489192d0149763e7cbe2f7f6f21536bc655
Akshat Bhardwaj <akshatbhardwaj019@gmail.com>
2024-08-23 18:01:00 +0530

    std acc2

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app=express();
 const port=5000
 app.get("/",(req, res)=>{
-    res.send("Welcome to hosel and fee managment")
+    res.send("Welcome to student accomodation")
 })
 app.listen(port,()=>{
     console.log("server started on port 9000")
